0a0e5d21088d882549d23659acc549c2be555f18
Vinod Soni <vinod.soni@microsoft.com>
2024-01-22 15:21:14 -0500

    correction in code

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddGrpc().AddJsonTranscoding();
 var app = builder.Build();
 
 
-app.MapGrpcService<GreeterService>();
+
 app.MapGrpcService<ToDoService>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
